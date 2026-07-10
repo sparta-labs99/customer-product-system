ff3ce5a9bd50c42c474931c49cc3bbfe5e2a17d4
YunJeongMin <wjdals2229@naver,com>
2026-07-10 17:28:07 +0900

    feat : ExceptionHandler 추가
    에러 핸들러 추가
    
    + EnableJpaAuditing 활성화 코드 추가

--- src/main/java/com/example/customerproductsystem/CustomerProductSystemApplication.java
+++ src/main/java/com/example/customerproductsystem/CustomerProductSystemApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
 @Slf4j
 @SpringBootApplication
+@EnableJpaAuditing
 public class CustomerProductSystemApplication {
 
     @Value("${spring.datasource.url}")
